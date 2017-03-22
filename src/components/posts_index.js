6877d0dec59acf37117b36110a72e0b10ab4041f
ivan <ivan.migov@gmail.com>
2017-03-22 22:09:41 +0200

    form constructing

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+// import {bindActionCreators} from 'redux';
+import {fetchPosts} from '../actions/index'
+import {Link} from 'react-router'
 
-export default ()=>{
-  return (
-    <div> List of blogs</div>
-  )
+class PostsIndex extends Component {
+  componentWillMount(){//about to render into the DOM, is called once
+   this.props.fetchPosts();
+  }
+
+  render(){
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a post
+          </Link>
+        </div>
+        List of blogs
+      </div>
+    )
+  }
 }
+
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({fetchPosts}, dispatch);
+// }
+
+// export default connect(null,mapDispatchToProps)(PostsIndex);
+export default connect(null,{fetchPosts})(PostsIndex);

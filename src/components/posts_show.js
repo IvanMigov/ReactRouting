9a2840dbafd060db5eccae1b715bd61c50f98077
ivan <ivan.migov@gmail.com>
2017-03-24 21:02:51 +0200

    before show detail

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,16 +1,25 @@
 import React, { Component,PropTypes } from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from '../actions/index'
+import {Link} from 'react-router'
 
 
 
 class PostsShow extends Component {
+  componentWillMount(){//about to render into the DOM, is called once
+    this.props.fetchPost(this.props.params.id);
+  }
+
   render(){
     return (
-      <div>Show post {this.props.params.id}</div>
+      <div>
+        this.props.params.id
+      </div>
     )
   }
 }
 
-export default PostsShow
+export default connect(null,{fetchPost})(PostsShow);
 
 
 
